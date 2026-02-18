544bfdd87bac9a7bc851b6e3768f99935220ad33
Gulshid Zada <gulshidzada4@gmail.com>
2026-02-18 19:14:18 +0500

    Var , let , const type variable done

---
+++ Basic fundamental/var_let_const.js
@@ -0,0 +1,21 @@
+// var , let, const 
+var a = 3;
+console.log(a);
+
+var a = 5;
+console.log(a);
+
+var a = 8;
+console.log(a)
+
+let x = 30;
+console.log(x);
+
+// let x = 23;
+// console.log(x);
+
+const pi_1 = 3.14;
+console.log(pi_1);
+
+pi_1 = 2.18838;
+console.log(pi_1);
