e7d66451354a53775cacbb8161232770fe839db2
Gulshid Zada <gulshidzada4@gmail.com>
2026-02-19 19:32:03 +0500

    rule for naming a variab;e

---
+++ Basic fundamental/variable_naming_rule.js
@@ -0,0 +1,24 @@
+// lecture #4:
+//      Topic : Rule for Naming Variable
+
+//1. Start with letter, _, or $
+var num = 3; // valid
+var _num = 4; // valid
+var $num = 10; // valid
+
+var $num_1 = 10; // valid 
+
+// var 34num = 34; // not valid / wrong
+
+//2. No spaces or special characters
+// var first value = 3; // not valid 
+var first_value = 3; //  valid 
+
+// _, $ = >  special character use
+// @, #, %,^,& etc  => special characters not use
+
+//3. Follow camelCase
+var firstName = "Gulshid Zada";
+
+//4. Avoid keywords
+// var switch = 3;  not valid
