6d26a32a49b32390924911b9f5a0d00cf564174a
Gulshid Zada <gulshidzada4@gmail.com>
2026-02-20 22:05:29 +0500

    Comment and Types of Comment in JavaScript done ==>

---
+++ Basic fundamental/comment.js
@@ -0,0 +1,17 @@
+// Lecture #5:
+    // Topic: Comment in JavaScript
+    // They are ignored by JavaScript and do not affect program execution.
+
+    // 1. Single line Comment
+    // This is Single line Comment
+    var a = 10; // a is variable it store 10
+
+
+    // 2. Multi Line Comment
+    /*
+
+    this is multi line Comment in js 
+    a = 20;
+    let y = 2;
+
+    */
