80f1265a4e1384391dc08ae3dee7371eb44fd1c3
Gulshid Zada <gulshidzada4@gmail.com>
2026-02-22 14:56:49 +0500

    Escape Sequences done

---
+++ Basic fundamental/Escape_Sequences.js
@@ -0,0 +1,22 @@
+// Lecture # 6: 
+    //  Topic : Escape Sequences in JavaScript
+    // Escape sequences are special characters used in strings, written with
+    //  a backslash, to represent formatting or special symbols.
+
+    // Escape Sequence	Meaning
+    //     \n	New line
+    //     \t	Tab
+    //     \\	Backslash
+    //     \'	Single quote
+    //     \"	Double quote
+    //     \r	Carriage return
+    //     \b	Backspace
+console.log("Hello\nWorld");
+
+console.log("This\tis \"java\\Script\" \'programming\'");
+
+console.log("JavaScript \r langauge");
+
+console.log("HEl\blo");
+
+
--- Basic fundamental/tempCodeRunnerFile.js
+++ Basic fundamental/tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-let b = 4;
-// let b = 3;
+World")
