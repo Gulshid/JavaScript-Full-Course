cf039e0d7920e87aaca68fdc4cd9668f5004e6d2
Gulshid Zada <gulshidzada4@gmail.com>
2026-02-24 22:09:32 +0500

    Datatype explained

---
+++ Basic fundamental/datatype.js
@@ -0,0 +1,68 @@
+/* lecture #7:
+    Topic: Datatype in JS
+var z = 2;
+var, let , const 
+var a = 4;
+JavaScript data types are divided into two categories:
+1️⃣ Primitive Data Types
+
+Primitive data types store single values and are immutable(Un-Changable).
+
+🔸 Number
+
+Used for numbers (integer & decimal).
+
+let age = 25; integer
+let price = 99.5; decimal / floating value
+🔸 String
+
+Used for text data.
+
+let name = "Ali";
+let city = 'Lahore';
+🔸 Boolean
+
+Used for true or false values.
+
+let isOnline = true;
+let isAdmin = false;
+🔸 Undefined
+
+A variable declared but not assigned a value.
+
+let x;
+🔸 Null
+
+Represents no value (intentionally empty).
+
+let data = null;
+🔸 BigInt
+
+Used for very large integers.
+
+let bigNumber = 12345678901234567890n;
+🔸 Symbol
+
+Used to create unique identifiers.
+
+let id = Symbol("id");
+
+2️⃣ Non-Primitive Data Types
+
+Non-primitive types store multiple values and are mutable(Changable).
+
+🔸 Object
+
+Used to store data in key–value pairs.
+
+let user = {
+    name: "Ali",
+    age: 20
+};
+🔸 Array
+
+Used to store multiple values in order.
+
+let colors = ["red", "green", "blue"];
+
+*/
