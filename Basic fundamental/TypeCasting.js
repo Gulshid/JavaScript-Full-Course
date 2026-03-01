c75683d8590d101e4580188f3aaf8617864d8b0e
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-01 14:58:41 +0500

    TYpe Casting in javaScript Started

---
+++ Basic fundamental/TypeCasting.js
@@ -0,0 +1,24 @@
+// ================================
+// JavaScript Type Conversion 
+// ================================
+
+
+/* Possibe Type Conversion
+1. Number converison
+    1. String to Number conversion
+    2. Boolean to Number conversion
+    3. Null to Number conversion
+
+2. String converison
+    1. Number  to String conversion
+
+3. Boolean converison
+    1. Number to Boolean conversion
+    2. String to Boolean conversion
+    3. Null to Boolean conversion
+    4. undefined to Boolean conversion
+    
+*/
+
+
+
