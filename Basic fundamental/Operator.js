0638b53a4e6b3212fa3ef1d1d5dc817aef3785d0
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-10 21:25:36 +0500

    OPerator in JavaScript start

---
+++ Basic fundamental/Operator.js
@@ -0,0 +1,32 @@
+/* 📘 Operators in JavaScript (Overview)
+
+Operators are symbols used to perform operations on values or variables.
+
+🔹 Types of Operators in JavaScript
+1️⃣ Arithmetic Operators
+
++ , - , * , / , % , **
+
+2️⃣ Assignment Operators
+
+= , += , -= , *= , /= , %= , **=
+
+3️⃣ Comparison Operators
+
+== , === , != , !== , > , < , >= , <=
+
+4️⃣ Logical Operators
+
+&& , || , !
+
+5️⃣ Unary Operators
+
+++ , -- , + , - , typeof
+
+6️⃣ Ternary Operator
+
+condition ? value1 : value2
+
+
+
+*/
---
+++ Basic fundamental/short_circuit_eval.js
@@ -0,0 +1,19 @@
+/*
+    Short-Circuit Evaluation
+
+Used with logical operators.
+
+&& and ||
+
+=============================
+    Operator Precedence
+
+Order in which operations are performed.
+
+Example:
+
+BODMAS Rule:
+    console.log(2 + 3 * 4);  // 14
+    console.log((2 + 3) * 4); // 20
+
+*/
