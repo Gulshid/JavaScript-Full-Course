8d0373c83928875d72988410b66fe3c87e6c6fb4
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-12 19:58:19 +0500

    Arthematic operator done means lecture 12 done

---
+++ Basic fundamental/Arithmetic_Operators.js
@@ -0,0 +1,22 @@
+/* Lecture # 12
+        Topic: Arthematic Operator in JavaScript
+
+1️ Arithmetic Operators
+
++ , - , * , / , % , **
+
+*/
+var x = 3;
+var y = 2;
+
+console.log("Addition :", x + y);
+
+console.log("Substraction :", x - y);
+
+console.log("Mulitplication :", x * y);
+
+console.log('Division :', x / y);
+
+console.log("Modulas :", x % y);
+
+console.log("Exponent :", x ** y); // 3 ^2 = 3 * 3 = 9
