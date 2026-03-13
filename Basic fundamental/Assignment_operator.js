e1d17b802dfa724b8ed915cc89a4901a600f66ab
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-13 22:16:17 +0500

    Arthematic and Assignment operator done

---
+++ Basic fundamental/Assignment_operator.js
@@ -0,0 +1,31 @@
+/*
+    Lecture # 13
+        Topic : Arthematic and Assignment Operator in Java Script
+2️ Arithmetic and  Assignment Operators
+
+ = , += , -= , *= , /= , %= , **= 
+
+ */
+
+let a = 2; // a = 2
+console.log("= :", a);
+
+a += 3; // a = 5
+console.log("+= :", a);
+
+a -= 4; // a = 1
+console.log("-= :", a);
+
+a *= 2; // a = 2
+console.log("*= :", a);
+
+a /= 2; // a = 1
+console.log("/= :", a);
+
+a %= 3; // a = 1
+console.log("%= :", a);
+
+a **= 2; // a = 1
+console.log("**= :", a);
+
+
