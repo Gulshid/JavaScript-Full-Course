2cca177c562ad56b08994421351141e05f23ac05
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-15 21:12:33 +0500

    Lecture 14 donee

---
+++ Basic fundamental/comparison_operator.js
@@ -0,0 +1,24 @@
+/*
+    Lecture #14
+        Topic : Comparison Operators in Java Script
+3️ Comparison Operators
+
+== , === , != , !== , > , < , >= , <=
+
+*/
+
+console.log(2 == 6); // false
+
+console.log(2 === "2"); // false
+
+console.log(3 != 2); // true
+
+console.log(10 !== "10"); // true
+
+console.log(10 > 5); // true
+
+console.log(10 < 5); // false
+
+console.log(2 >= 4); // true
+
+console.log(3 <= 5); // true
