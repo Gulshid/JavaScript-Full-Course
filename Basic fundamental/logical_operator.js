b421ae1062a9e161c67da3e57e0cdaa44ce4c31e
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-16 19:55:35 +0500

    Logical operator / lecture 15 done

---
+++ Basic fundamental/logical_operator.js
@@ -0,0 +1,17 @@
+/*
+    Lecture #15
+        Topic: Logical Operators in JavaScript
+4️ Logical Operators
+
+&& , || , ! =>  (AND, OR, NOT)
+==========================
+*/
+// AND OPERATOR 
+console.log(10 > 4 && 4 < 6); // true , true => true |/ Otherwise false
+
+
+// OR OPERATOR 
+console.log(2 > 1 || 5 < 10); // true , false => true |/ Otherwise false
+
+// NOT OPERATOR 
+console.log(!3 < 2); // true => false | false => true
