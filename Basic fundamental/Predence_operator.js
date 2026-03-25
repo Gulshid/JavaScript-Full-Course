7c2811cd54ed38a83e75abf8551043f06bd19934
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-25 21:57:04 +0500

    Predenece of operator done

---
+++ Basic fundamental/Predence_operator.js
@@ -0,0 +1,22 @@
+/*
+
+Lecture #18
+    Topic :   Precedence of Operator in JavaScript
+
+
+Order in which operations are performed.
+
+Example:
+
+BODMAS Rule:
+    1.  (), [], {}
+    2. / , *
+    3. +, -
+    console.log(2 + 3 * 4);  // 14 / 20
+    console.log((2 + 3) * 4); // 20 / 14
+
+*/
+
+console.log(2 + 3 * 4);
+
+console.log((2 + 3) * 4);
--- Basic fundamental/short_circuit_eval.js
+++ Basic fundamental/short_circuit_eval.js
@@ -6,14 +6,4 @@ Used with logical operators.
 && and ||
 
 =============================
-    Operator Precedence
-
-Order in which operations are performed.
-
-Example:
-
-BODMAS Rule:
-    console.log(2 + 3 * 4);  // 14
-    console.log((2 + 3) * 4); // 20
-
 */
