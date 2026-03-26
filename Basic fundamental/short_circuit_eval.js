45c5171672510437cf05a15e8ff35d0e1c59a6cf
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-26 07:12:54 +0500

    SHort circuit eval done

--- Basic fundamental/short_circuit_eval.js
+++ Basic fundamental/short_circuit_eval.js
@@ -1,5 +1,10 @@
 /*
-    Short-Circuit Evaluation
+Lecture # 19
+    Topic : Short Circuit Evaluation in JavaScript
+    Short-Circuit Evaluation:
+        Short-circuit evaluation is a behavior of logical operators
+        (&& and ||) where JavaScript stops 
+        evaluating further expressions as soon as the result is determined.
 
 Used with logical operators.
 
@@ -7,3 +12,25 @@ Used with logical operators.
 
 =============================
 */
+
+// && logical Operator 
+console.log("=======Logical &&=========");
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+console.log("==== short circuit evaluation && :");
+console.log(false && "Hello"); // false
+
+console.log("================");
+
+// || logical Operator 
+console.log("=======Logical ||=========");
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log("==== short circuit evaluation || :");
+console.log(false || "World"); // "World"
