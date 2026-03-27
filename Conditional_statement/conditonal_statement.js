ea1cd34d2eae309c1a607afadf402d44d37b7ee5
Gulshid Zada <gulshidzada4@gmail.com>
2026-03-27 12:57:04 +0500

    Conditional Statement Chapter start

---
+++ Conditional_statement/conditonal_statement.js
@@ -0,0 +1,40 @@
+/* 
+4. Conditional Statements
+
+Conditional statements are used to make decisions in code based on conditions.
+
+    Types of Conditional Statements
+1️ if Statement
+
+if (condition){}
+=========================
+2️ if-else
+
+if (condition) { } else { }
+=========================
+3️ else-if Ladder
+if (condition1) { }
+else if (condition2) { }
+else { }
+=========================
+4️ Nested Conditions
+if (condition) {
+    if (anotherCondition) { }
+}
+=========================
+5️ switch Statement
+switch(value) {
+    case x:
+        break;
+    default:
+}
+
+=========================
+6️ Real-world Logic Building Examples
+Age check
+Login system
+Grade system
+Even/Odd check
+Discount calculation
+=========================
+*/
