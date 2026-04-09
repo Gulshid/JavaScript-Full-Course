c03df33631208c4066310d0e7d61d4ebabad1a97
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-09 21:30:10 +0500

    for loop done

---
+++ loops/for_loop.js
@@ -0,0 +1,31 @@
+console.log("1 ---- 10 :");
+for(let a = 1; a <= 10; a++)
+{
+    console.log(a , " ");
+}
+
+console.log("============");
+console.log("10 ---- 1 :");
+for (let b = 10; b >= 1; b--)
+{
+    console.log(b , " ");
+}
+
+/*
+Lecture # 25
+    Topic : Loop and => for Loop in JavaScript
+
+    LOOP :- A way to repeat a block of code multiple times until a condition is met.
+
+
+    1.  for LOOP : Used when you know how many times to run the loop.
+                        let a = 2;      a < 10;  a++/ a--
+        SYNTAX : for (initialization; condition; update ) {
+                    code to run
+                    }
+
+        initialization → start value (runs once)
+        condition → check before each loop
+        update → change value after each loop
+
+*/
