99b7cc520bc20a4326b2de1514faa66caa953dfa
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-10 16:27:50 +0500

    while loop done

---
+++ loops/while_loop.js
@@ -0,0 +1,33 @@
+console.log("while loop ----- 1 ---- 10 :");
+let x = 1;
+
+while(x <= 10)
+{
+    console.log(x);
+    x = x + 1;
+}
+
+console.log("=============");
+console.log("while loop ----- 10 ---- 1 :");
+
+let z = 10;
+
+while(z >= 1)
+{
+    console.log(z);
+    z = z - 1;
+}
+/*
+Lecture # 26:
+    Topic : While loop in JavaScript
+
+    while Loop: Runs while a condition is true.
+
+
+    SYNTAX :
+    initialization; ==> starting point 
+    while (condition) {
+        code to run
+        update; ==> increment / decrement 
+}
+*/
