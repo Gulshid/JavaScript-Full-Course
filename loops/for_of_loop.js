338b128768647c5027c8b5fc9bf3ee3ec8d8e949
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-13 21:13:08 +0500

    for of loop done

---
+++ loops/for_of_loop.js
@@ -0,0 +1,23 @@
+const Marks = [10, 20, 30, 40, 50];
+
+// Use the for - of loop
+for (let x of Marks)
+{
+    console.log(x);
+}
+
+/*
+Lecture # 28
+    Topic : for - of loop in JavaScript
+
+    for...of Loop:
+        Used for arrays, strings, iterable objects.
+
+    Syntax :
+
+    const a = [1,2,3];
+        for (let arr of a) {
+            console.log(arr);
+            }
+
+*/
