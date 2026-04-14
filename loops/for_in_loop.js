861e5989350a337961320ff146bf1746b1f16483
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-14 19:53:59 +0500

    for in loop in JavaScript

---
+++ loops/for_in_loop.js
@@ -0,0 +1,18 @@
+/*
+Lecture # 29
+    Topic : for in Loop in JavaScript
+
+    5. for...in Loop :
+        Used for object properties (keys).
+
+    Syntax :
+        const user = {
+        name: "Ali",
+        age: 22
+        };
+
+    for (let key in user) {
+        console.log(key, user[key]);
+        }
+
+*/
