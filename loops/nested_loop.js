7d0e069d6763fe3b666a907a34c60764177740cf
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-15 18:00:18 +0500

    Nested loop in JavaScript

---
+++ loops/nested_loop.js
@@ -0,0 +1,25 @@
+for(let a = 1; a <= 3; a++)
+{
+    for(let b = 1; b <= 2; b++)
+    {
+        console.log(a + " " + b);
+    }
+
+}
+/*
+Lecture # 30 
+    Topic : Nested Loop in JavaScript
+
+    6. Nested Loop :
+        Loop inside another loop.
+            -   Used in patterns, matrices, grids
+
+    Syntax :
+        for(let a = 1; a <= 3; a++) ==> outside loop
+        {
+            for(let b = 1; b <=3; b++) ===> inside loop
+            {
+
+            }
+        }
+*/
