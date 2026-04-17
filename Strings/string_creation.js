1f1c21fee6b111c60b2934848423c030642356d1
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-17 22:49:18 +0500

    String creation done

---
+++ Strings/string_creation.js
@@ -0,0 +1,17 @@
+let a = 'Hello 1 2 # $ / -===-3=3+993939'; // Method # 1
+let b = "Hello 1 2 #nkjskjlslsll"; // Method # 2
+let c = `jjljallaljja0o3llls;nsnns`; // Method # 3
+
+console.log(a);
+console.log("============");
+console.log(b);
+console.log("============");
+console.log(c);
+/*
+Lecture # 32:
+    Topic : String Creation in JavaScript
+
+    String :
+        A string is a sequence of characters (text).
+
+*/
