aa6278d64595942e0adf60e92cebcb9fa0c2a756
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-18 16:31:27 +0500

    lecture 33 done

---
+++ Strings/template_literal.js
@@ -0,0 +1,43 @@
+let name = "Gulshid Zada";
+let age = 23;
+
+console.log("Name :", name);
+console.log("=========");
+console.log("Age :", age);
+
+console.log("=====================");
+console.log(`Name : ${name} , Age : ${age}`);
+
+console.log("=====================");
+
+let value = `
+        this is 
+        string of
+        javaScript language
+
+`;
+
+console.log(value);
+/*
+Lecture # 33
+    Topic : Template Literals in JavaScript
+
+    Template Literals:
+        - Template literals in JavaScript are a modern way to work with strings.
+        - Template literals use backticks (` `) and allow variables inside strings.
+
+        Example : 
+        let a = 2;
+            console.log(a);
+            console.log(`${a}`);
+
+    Benefits:
+        -  Multi-line strings
+
+        let b = `
+        this is 
+        string of
+        javaScript language
+        `;
+
+*/
