b6579aa8b3ac3f54a1d809fb57382a437e4711e6
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-19 18:34:16 +0500

    string methods in javaScript

---
+++ Strings/string_methods.js
@@ -0,0 +1,43 @@
+//1.  Length function / Method
+let x = "JavaScript";
+
+console.log(x.length);
+console.log("=================");
+//2. slice() function / Method
+console.log(x.slice(0, 10));
+console.log(x.slice(-6)); // Negative value allow 
+console.log("=================");
+
+//3.  substring() function / Method
+console.log(x.substring(0, 10));
+console.log(x.substring(-6)); //does not allow Negative value  
+console.log("=================");
+
+//4.  replace() function / Method
+let text = "Hello World";
+console.log(text);
+console.log(text.replace("World", "Ali"));
+console.log("=================");
+
+//5.  includes(), function / Method
+let value = "Hello WOrld";
+console.log(value.includes("Hello"));
+console.log("=================");
+//6. split(), function / Method
+
+
+//7. trim() function / Method
+
+//8. toUpperCase() function / Method
+
+//9.  toLowerCase() function / Method
+/*
+Lecture # 34
+    Topic : String Methods in JavaScript
+
+    String Methods:
+        length, slice(), substring(), replace()
+        includes(), split(), trim(), 
+        toUpperCase(), toLowerCase()
+
+*/
