fe8663662734971eddf1fc668f39f839be4f8258
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-20 17:58:23 +0500

    String Immutability done

---
+++ Strings/string_immutable.js
@@ -0,0 +1,19 @@
+let word = "Hello";
+console.log(word);
+
+word[0] = "h";
+console.log(word);
+
+console.log("===============");
+word = "World";
+console.log(word);
+
+/*
+Lecture # 35
+    Topic : String Immutability in JavaScript
+
+
+    String Immutability:
+        Strings in JavaScript are immutable (cannot be changed).
+
+*/
