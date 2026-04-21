f21862f44739449888e7b118f2a83fbaf0e91f2f
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-21 18:11:35 +0500

    Creating array done

---
+++ Array/creating_array.js
@@ -0,0 +1,34 @@
+let fruite = ["Apple", "Banana", "Peach"];
+// let fruite_1 = "Apple";
+// let fruite_2 = "Banana";
+// let fruite_3 = "Peach";
+
+console.log("Fruite :", fruite);
+console.log("===========");
+
+let numbers = [1,2,3,4,5];
+console.log("Numbers :", numbers);
+console.log("===========");
+
+let value = [1, 2.4, "Hello world",  true];
+console.log("Multiple type values :", value);
+console.log("===========");
+
+let arr = [];
+console.log("Empty Array :", arr);
+
+
+/*
+Lecture # 36:
+    Topic : Creating Array in JavaScript
+
+    Creating Arrays:
+        Arrays store multiple values in one variable.
+
+
+        example :
+            let a = 3;
+
+
+            let b = [1, 2, "hello", true];
+*/
