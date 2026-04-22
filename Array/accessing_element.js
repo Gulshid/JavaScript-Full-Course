dc2497ce133818820565d53e084b42b12d961795
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-22 17:56:53 +0500

    accessing_element done

---
+++ Array/accessing_element.js
@@ -0,0 +1,14 @@
+let arr = [1,2,2.5, "Hello", true];
+
+console.log(arr[0], arr[2], arr[1], arr[3], arr[4]);
+/*
+Lecture # 37
+    Topic : Accessing Elements in JavaScript
+
+
+    Accessing Elements:
+        Each item has an index (starting from 0).
+                (0,1,2)
+    let arr = [1,2,3];
+    console.log(arr[0], arr[1]);
+*/
