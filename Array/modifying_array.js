029a5c20539439f39501f18888ba601719b7b255
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-23 18:05:38 +0500

    modifying array done

---
+++ Array/modifying_array.js
@@ -0,0 +1,21 @@
+let fruits = ["Apple", "Peach", "Mango"];
+
+console.log("Before modifying :", fruits);
+
+fruits[0] = "Peach";
+fruits[1] = "Apple";
+fruits[2] = "Orange";
+console.log("After modifying :", fruits);
+/*
+Lecture # 38
+    Topic : Modifying Arrays in javaScript
+
+    Modifying Arrays:
+        You can change values using index
+
+    let fruits = ["apple", "peach", "mango"];
+    console.log(fruits); ==> ["apple", "peach", "mango"];
+
+    fruits[1] = "orange";
+    console.log(fruits);   ==> ["apple", "orange", "mango"];
+*/
