32f9987085bb3a656625acfdbe692217a270be2a
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-24 18:42:04 +0500

    length of array and push , pop method done

---
+++ Array/array_length_push_pop.js
@@ -0,0 +1,30 @@
+let fruits = ["Apple", "Mango", "Orange", "Banana"];
+console.log(fruits);
+console.log("=============");
+
+// Array Length
+console.log("The length of Fruits Array :",fruits.length);
+console.log("=============");
+
+// push() Method 
+fruits.push("Graps");
+console.log(fruits);
+console.log("The length of Fruits Array :",fruits.length);
+console.log("=============");
+
+// pop() Method 
+fruits.pop();
+console.log(fruits);
+console.log("The length of Fruits Array :",fruits.length);
+/*
+    Lecture # 39
+        Topic : Array Length & push() and pop() in JavaScript
+
+        Array Length:
+            Returns total number of elements
+
+        push() and pop():
+            push() → add element at end
+            pop() → remove last element
+
+*/
