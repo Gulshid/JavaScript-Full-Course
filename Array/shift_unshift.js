23367e2c3cdb46a8e66c6e2f7f6737fd5a530d0f
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-25 19:51:36 +0500

    shift and unshift

---
+++ Array/shift_unshift.js
@@ -0,0 +1,21 @@
+let numbers = [1, 2, 3, 4, 5];
+
+console.log("Number :", numbers);
+console.log("==============");
+
+// shift() → remove first element
+numbers.shift();
+console.log("Number :", numbers);
+console.log("==============");
+
+// unshift() → add element at beginning
+numbers.unshift(1);
+console.log("Number :", numbers);
+/*
+Lecture # 40
+    Topic : Shift and UnShift in JavaScript
+
+    shift() → remove first element
+    unshift() → add element at beginning
+
+*/
