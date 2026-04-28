a1030733daa686917bd9311e42db2b4322e52b02
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-28 17:55:55 +0500

    function chapter start

---
+++ function/function_declaration.js
@@ -0,0 +1,32 @@
+function name() // ===> function declaration
+{
+    // function definition
+    console.log("This is the first program of function ****")
+    console.log("Gulshid Zada");
+    console.log("function");
+
+}
+
+
+// function call
+name();
+
+/*
+Lecture # 43
+    Topic : function in JavaScript
+
+    function : 
+        A function in JavaScript is a block of code that performs a specific task.
+
+    Function Declaration: A normal function created using the function keyword.
+
+        parts of  function :~
+        * function declaration
+        function greet()
+        * function definition
+        function greet(){
+            function definition
+        }
+        * function call
+        greet();
+*/
