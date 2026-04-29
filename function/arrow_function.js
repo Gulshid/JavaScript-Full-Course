17a32a76238b91629ba6c8eb4e55f56b3fb4fa8b
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-29 17:14:13 +0500

    function expression and arrow function done

---
+++ function/arrow_function.js
@@ -0,0 +1,19 @@
+const arrow = () =>{
+    let z = 2.4;
+    console.log("Arrow function");
+    console.log(z);
+}
+
+arrow();
+/*
+Lecture # 44
+    Topic : Arrow function in JavaScript
+
+
+    Arrow Functions:
+        Short syntax for writing functions.
+
+    const greet = () => {
+    console.log("Hello");
+    };
+*/
---
+++ function/function_Expression.js
@@ -0,0 +1,23 @@
+const x = function Expression()
+{
+    let a = 5;
+    console.log("Function Expression");
+    console.log(a);
+}
+
+// Expression();
+x();
+
+/*
+Lecture # 44
+    Topic : function Expression in JavaScript
+
+    function Expression:
+        A function stored inside a variable.
+
+    2x + 3 = 0 ==> Expression
+    const greet = function() {
+    console.log("Hello");
+    };
+
+*/
