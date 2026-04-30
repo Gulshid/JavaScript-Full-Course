6d932af028c22138b56b37d31a0dc3fe5e20986a
Gulshid Zada <gulshidzada4@gmail.com>
2026-04-30 18:10:58 +0500

    lecture 45 done

---
+++ function/default_paramter.js
@@ -0,0 +1,15 @@
+function greet(name = "Pakistan"){
+    console.log("Hello " + name);
+}
+
+greet("Gulshid");
+greet();
+
+/*
+Lecture # 45
+    Topic : Default Parameters in JavaScript
+
+
+    Default Parameters:
+        Provide default values if no argument is passed.
+*/
---
+++ function/paramter_return.js
@@ -0,0 +1,17 @@
+function add(a,b){ // ===> paramter
+    return a + b;
+}
+
+console.log(add(5,14)); // ===> Argument
+/*
+Lecture # 45
+    Topic : Paramters & return in JavaScript
+
+
+    Parameters & Return:
+        Parameters receive values, and return sends back output.
+
+    key term related to function :
+        * Paramter ==> variable (a,b,x,y,z == a -- z)
+        * Argument ==> constant (1,2,3 === (0 -- 9))
+*/
