fe3a35bc1a521cd6bf7a5f127edb4911f23b0883
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-04 17:49:49 +0500

    Object creation done

---
+++ Object/object_creation.js
@@ -0,0 +1,15 @@
+const users = {
+    name : "Gulshid Zada",
+    age : 23,
+    marks : 500
+}
+
+console.log(users);
+/*
+Lecture # 49
+    Topic : Object Creation in JavaScript
+
+
+    Object Creation:
+        Object is a collection of key-value pairs.
+*/
