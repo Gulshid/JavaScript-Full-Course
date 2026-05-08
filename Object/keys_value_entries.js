342cd0068b5eff8ad1636dfb0ac5d49225854ddf
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-08 18:22:50 +0500

    lecture 53 done

---
+++ Object/keys_value_entries.js
@@ -0,0 +1,19 @@
+const user = {
+    name : "Ali",
+    age : 20
+};
+
+
+console.log(Object.keys(user)); // only keys ==> fetch
+console.log(Object.values(user)); // only values ==> fetch
+console.log(Object.entries(user)); // both keys and values fetch
+
+
+
+/*
+Lecture # 53
+    Topic : Object.keys(), values(), entries() in JavaScript
+
+    Object.keys(), values(), entries():
+       *  Get data from object
+*/
