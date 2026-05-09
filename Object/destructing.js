76a62b9b1ae0a4aa16b4dfe691cc94d78fd4543d
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-09 18:17:39 +0500

    lecture 54 done

---
+++ Object/destructing.js
@@ -0,0 +1,19 @@
+const user = {
+    name : "Gulshid Zada",
+    age : 23
+};
+
+// Destructing method
+const {name, age} = user;
+console.log(name, age);
+
+
+// console.log(user);
+
+/*
+Lecture # 54
+    Topic : Destructuring in JavaScript
+
+    Destructuring:
+        * Extract values easily
+*/
