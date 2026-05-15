500ce1f98c4429c3eaa8318ae36a054e9ca3d23f
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-15 16:58:54 +0500

    lecture 60 done

---
+++ OOP/inheritence.js
@@ -0,0 +1,24 @@
+class Animals{
+    sound()
+    {
+        console.log("Animals make a sound");
+    }
+
+}
+
+class Dog extends Animals{
+
+}
+
+const d1 = new Dog();
+d1.sound();
+
+
+/*
+Lecture # 60
+    Topic : Inheritance in JavaScript
+
+    Inheritance:
+        Inheritance means one class can use 
+        properties and methods of another class.
+*/
