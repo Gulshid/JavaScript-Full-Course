16d3fe145794a85002c7ef1b354aab55bd7d038d
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-22 18:44:49 +0500

    lecture 63 done

---
+++ OOP/Static_Methods.js
@@ -0,0 +1,15 @@
+class MathTest{
+    static add(a,b){
+        return a + b;
+    }
+}
+
+// const m1 = new MathTest();
+console.log(MathTest.add(6,3));
+/*
+Lecture # 63
+    Topic : Static Methods in JavaScript
+
+    Static Methods:
+        Static methods belong to the class, not objects.
+ */
