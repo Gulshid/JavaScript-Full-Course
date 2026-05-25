55f081b244a192ea3c1452e9a431d495162f3040
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-25 18:59:58 +0500

    try catch done

---
+++ Exception Handling/try_catch.js
@@ -0,0 +1,16 @@
+try{
+    console.log(a);
+
+}catch(error){
+    console.log("a is not Defined");
+}
+/*
+Lecture # 66
+    Topic : try Catch in JavaScript
+
+    try:
+        try is used to test code that may cause an error.
+
+    catch:
+        catch handles the error if an error happens inside try.
+*/
