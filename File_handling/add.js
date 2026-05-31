3c691d4bfa34f41ec7a362906bc42bc734530947
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-31 19:18:57 +0500

    module in javascript

---
+++ File_handling/add.js
@@ -0,0 +1,2 @@
+const add = require("./math");
+console.log("The add of two value is :", add(3,4));
---
+++ File_handling/module.js
@@ -0,0 +1,7 @@
+/*
+Lecture # 69
+    Topic : Modules in JavaScript
+
+    Modules:
+        Modules are separate files that contain code and can be reused in other files.
+*/
