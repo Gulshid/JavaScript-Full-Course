7c15dc1c859ed34c25321d661847e084e71ea569
Gulshid Zada <gulshidzada4@gmail.com>
2026-06-01 18:47:49 +0500

    lec 70 done

---
+++ File_handling/add_1.js
@@ -0,0 +1,2 @@
+import { add }  from "./math_1.js";
+console.log("The add of two value is :", add(2,8));
---
+++ File_handling/common_js_ES_Modules.js
@@ -0,0 +1,10 @@
+/*
+Lecture # 70 
+    Topic : CommonJS (require) & ES Modules (import/export) in JavaScript
+
+    CommonJS (require):
+        CommonJS is the old/default way in Node.js to import modules using require().
+
+    ES Modules (import/export):
+        ES Modules are the modern JavaScript way to import/export files using import and export.
+*/
